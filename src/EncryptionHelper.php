ed7a4ecb1c0db501aeeafc63b48aa6db7444f92a
Michael Cummings <mgcummings@yahoo.com>
2016-10-25 14:54:45 -0700

    Rename properties from the confusing ones used in original c# code to ones more in tune with PHP. Add methods to manage query content that would have been possible through the Dictionary class in the c# code. Added methods and properties to make managing the encryption cipher and other useful cipher parameters to allow for possible future changes to what is used.

--- src/EncryptionHelper.php
+++ src/EncryptionHelper.php
@@ -23,24 +23,26 @@ class EncryptionHelper
      * to compare with original c# version.
      *
      * @param string $encryptedData
-     * @param string $keyString
-     * @param string $keyBytes
+     * @param string $key
+     * @param string $initVector
      *
-     * @uses EncryptionHelper::setKeyBytes()
-     * @uses EncryptionHelper::setKeyString()
+     * @uses EncryptionHelper::setInitVector()
+     * @uses EncryptionHelper::setKey()
      * @uses EncryptionHelper::decrypt()
      * @uses EncryptionHelper::computedCheckSum()
+     * @throws \RangeException
+     * @throws \RuntimeException
      */
     public function __construct(
         string $encryptedData,
-        string $keyString = 'ABC12345',
-        string $keyBytes = "\x11\x12\x13\x14\x15\x16\x17\x18"
+        string $key = 'ABC12345',
+        string $initVector = "\x11\x12\x13\x14\x15\x16\x17\x18"
     ) {
-        $this->setKeyBytes($keyBytes)
-             ->setKeyString($keyString);
+        $this->setInitVector($initVector)
+             ->setKey($key);
         // Decrypt string
         $data = $this->decrypt($encryptedData);
-        // Parse out key/value pairs and add to dictionary
+        // Parse out name/value pairs and add to dictionary
         $checksum = null;
         $args = explode('&', $data);
         /**
@@ -49,34 +51,35 @@ class EncryptionHelper
         foreach ($args as $arg) {
             $i = strpos($arg, '=');
             if (false !== $i) {
-                $key = substr($arg, 0, $i);
+                $name = substr($arg, 0, $i);
                 $value = substr($arg, $i + 1);
-                if ($key === $this->checksumKey) {
+                if ($name === $this->checksumName) {
                     $checksum = $value;
                 } else {
-                    $this->dictionary[urldecode($key)] = urldecode($value);
+                    $this->queries[urldecode($name)] = urldecode($value);
                 }
             }
         }
         if ($checksum === null || $checksum !== $this->computedCheckSum()) {
-            $this->dictionary = [];
+            $this->queries = [];
         }
     }
     /**
+     * Returns query string using current contents.
+     *
      * @return string
      * @uses EncryptionHelper::computedCheckSum()
      */
     public function __toString()
     {
-        // Build query string from current contents
         /*
          * Example of my original line for line conversion from c# to have for comparision to final code.
          * $content = '';
-         * foreach (array_keys($this->dictionary) as $key) {
+         * foreach (array_keys($this->dictionary) as $name) {
          *     if (strlen($content) > 0) {
          *          $content .= '&';
          *     }
-         *     $content .= sprintf('%s=%s', urlencode($key), urlencode($this->dictionary[$key]));
+         *     $content .= sprintf('%s=%s', urlencode($name), urlencode($this->dictionary[$name]));
          * }
          * if (strlen($content) > 0) {
          *     $content .= '&';
@@ -85,11 +88,21 @@ class EncryptionHelper
          * return $content;
          */
         $content = [];
-        foreach ($this->dictionary as $key => $value) {
-            $content[] = sprintf('%s=%s', urlencode($key), urlencode($value));
+        foreach ($this->queries as $name => $value) {
+            $content[] = sprintf('%s=%s', urlencode($name), urlencode($value));
         }
-        $content[] = sprintf('%s=%s', $this->checksumKey, $this->computedCheckSum());
+        $content[] = sprintf('%s=%s', $this->checksumName, $this->computedCheckSum());
         return implode('&', $content);
+    }
+    /**
+     * Allows add new queries to contents.
+     *
+     * @param string $name
+     * @param string $value
+     */
+    public function addQuery(string $name, string $value)
+    {
+        $this->queries[$name] = $value;
     }
     /**
      * Used to decrypt an encode string.
@@ -104,8 +117,8 @@ class EncryptionHelper
      */
     public function decrypt(string $text): string
     {
-        $keyData = substr($this->keyString, 0, 8);
-        $decrypted = mcrypt_decrypt(MCRYPT_DES, $keyData, $this->getBytes($text), MCRYPT_MODE_CBC, $this->keyBytes);
+        $decrypted = mcrypt_decrypt($this->cipher, $this->getKey(), $this->getBytes($text), MCRYPT_MODE_CBC,
+            $this->getInitVector());
         if (false === $decrypted) {
             return '';
         }
@@ -125,41 +138,120 @@ class EncryptionHelper
      */
     public function encrypt(string $text): string
     {
-        $keyData = substr($this->keyString, 0, 8);
         $text = $this->addPadding($text);
-        $encrypted = mcrypt_encrypt(MCRYPT_DES, $keyData, $text, MCRYPT_MODE_CBC, $this->keyBytes);
+        $encrypted = mcrypt_encrypt($this->cipher, $this->getKey(), $text, MCRYPT_MODE_CBC, $this->getInitVector());
         if (false === $encrypted) {
             return '';
         }
         return $this->getString($encrypted);
-//        return $encrypted;
     }
     /**
-     * Used to set binary string value for 'iv'.
+     * Check if a named query exists in content.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasQuery(string $name)
+    {
+        return array_key_exists($name, $this->queries);
+    }
+    /**
+     * Allows remove existing query from content.
+     *
+     * @param string $name
+     *
+     * @return bool Returns true if query existed and was removed else false.
+     */
+    public function removeQuery(string $name)
+    {
+        if (array_key_exists($name, $this->queries)) {
+            unset($this->queries[$name]);
+            return true;
+        }
+        return false;
+    }
+    /**
+     * @param string $value
+     *
+     * @return $this Fluent interface
+     * @throws \RangeException
+     */
+    public function setCipher(string $value = MCRYPT_DES)
+    {
+        if (!in_array($value, mcrypt_list_algorithms(), true)) {
+            $message = sprintf('Cipher %s is not known', $value);
+            throw new \RangeException($message);
+        }
+        $current = $this->cipher;
+        try {
+            $this->setKey($this->key)
+                 ->setInitVector($this->initVector);
+        } catch (\Exception $exc) {
+            $this->cipher = $current;
+            $message = 'Cipher could not be changed because either current key or initialization vector are not'
+                . ' compatible with new cipher';
+            throw new \RangeException($message, 1, $exc);
+        }
+        $this->cipher = $value;
+        return $this;
+    }
+    /**
+     * Used to set binary string value for initialization vector.
      *
      * Through not in the original c# class this makes it nicer to re-use in other ways.
+     *
+     * NOTE: Use of a plain text vector is not a good idea. It is much better to use some sort of binary string.
      *
      * @param string $value
      *
      * @return $this Fluent interface
+     * @throws \RangeException
+     * @throws \RuntimeException
      */
-    public function setKeyBytes(string $value)
+    public function setInitVector(string $value)
     {
-        $this->keyBytes = $value;
+        $size = mcrypt_get_iv_size($this->cipher, MCRYPT_MODE_CBC);
+        if (false === $size) {
+            $message = 'Failed to get required vector size';
+            throw new \RuntimeException($message);
+        }
+        if ($size > strlen($value)) {
+            $message = sprintf('Initialization vector must be at least %s characters long', $size);
+            throw new \RangeException($message);
+        }
+        $this->ivSize = $size;
+        $this->initVector = $value;
         return $this;
     }
     /**
      * Used to set binary string value for the 'key'.
      *
+     * Value will be truncated to correct size for the cipher used.
+     *
+     * NOTE: Use of a plain text key is not a good idea. It is much better to use some sort of binary string key.
+     *
      * Through not in the original c# class this makes it nicer to re-use in other ways.
      *
      * @param string $value
      *
      * @return $this Fluent interface
+     * @throws \RangeException
+     * @throws \RuntimeException
      */
-    public function setKeyString(string $value)
+    public function setKey(string $value)
     {
-        $this->keyString = $value;
+        $size = mcrypt_get_key_size($this->cipher, MCRYPT_MODE_CBC);
+        if (false === $size) {
+            $message = 'Failed to get required key size';
+            throw new \RuntimeException($message);
+        }
+        if ($size > strlen($value)) {
+            $message = sprintf('Key must be at least %s characters long', $size);
+            throw new \RangeException($message);
+        }
+        $this->keySize = $size;
+        $this->key = $value;
         return $this;
     }
     /**
@@ -171,7 +263,9 @@ class EncryptionHelper
      */
     private function addPadding(string $data): string
     {
-        $padding = 8 - (strlen($data) % 8);
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        $size = mcrypt_get_block_size($this->cipher, MCRYPT_MODE_CBC);
+        $padding = $size - (strlen($data) % $size);
         return $data . str_repeat(chr($padding), $padding);
     }
     /**
@@ -189,8 +283,8 @@ class EncryptionHelper
             }
             return $acc;
         };
-        $checksum += array_reduce(array_keys($this->dictionary), $func);
-        $checksum += array_reduce($this->dictionary, $func);
+        $checksum += array_reduce(array_keys($this->queries), $func);
+        $checksum += array_reduce($this->queries, $func);
         return sprintf('%X', $checksum);
     }
     /**
@@ -209,6 +303,20 @@ class EncryptionHelper
             $results .= chr(hexdec($chunk));
         }
         return $results;
+    }
+    /**
+     * @return string
+     */
+    private function getInitVector(): string
+    {
+        return substr($this->initVector, 0, $this->ivSize);
+    }
+    /**
+     * @return string
+     */
+    private function getKey(): string
+    {
+        return substr($this->key, 0, $this->keySize);
     }
     /**
      * @param string $data
@@ -232,9 +340,11 @@ class EncryptionHelper
      */
     private function removePadding(string $data): string
     {
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        $size = mcrypt_get_block_size($this->cipher, MCRYPT_MODE_CBC) + 1;
         $padding = substr($data, -1);
-        // Pads to next block so no more than 8 chars.
-        if (9 >= ord($padding)) {
+        // Pads to next block so no more than $size chars.
+        if ($size >= ord($padding)) {
             return rtrim($data, $padding);
         }
         return $data;
@@ -242,34 +352,50 @@ class EncryptionHelper
     /**
      * Name for checksum value (unlikely to be used as arguments by user)
      *
-     * @var string $checksumKey
+     * @var string $checksumName
      */
-    private $checksumKey = "__$$";
+    private $checksumName = "__$$";
+    /**
+     * @var string $cipher Which cipher is being used.
+     */
+    private $cipher = MCRYPT_DES;
+    /**
+     * Initialization vector use for encryption and decryption.
+     *
+     * Change the following vector to ensure uniqueness
+     * Must be correct length for the cipher used.
+     *
+     * Originally was going to use array like from c# but string is easier to use direct in mcrypt.
+     * $_keyBytes = [0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18];
+     *
+     * @internal string $keyBytes Original c# code called it _keyBytes and had a _keyString as well which was
+     * confusing so renamed them both to match terms used in PHP documentation.
+     * @var string $initVector
+     */
+    private $initVector;
+    /**
+     * @var int $ivSize Length of initialization vector for current cipher.
+     */
+    private $ivSize;
+    /**
+     * Must be at least as long as the used cipher's minimum key size.
+     *
+     * @internal string $keyString Original c# code called it _keyString and had a _keyBytes as well which was
+     * confusing so renamed them both to match terms used in PHP documentation.
+     * @var string $key
+     */
+    private $key;
+    /**
+     * @var int $keySize Length of key for current cipher.
+     */
+    private $keySize;
     /**
      * Hold associate array of the query string keys and values.
      *
      * In the original c# code the class had to extend from another template class just to get something like this.
      * Makes you really appreciate how easy some things are in PHP.
      *
-     * @var array $dictionary
+     * @var array $queries
      */
-    private $dictionary = [];
-    /**
-     * Change the following key to ensure uniqueness
-     * Must be 8 bytes
-     *
-     * Originally was going to use array like from c# but string is easier to use direct in mcrypt.
-     * $_keyBytes = [0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18];
-     *
-     * This is better know as an 'iv' in PHP etc.
-     *
-     * @var string $keyBytes
-     */
-    private $keyBytes;
-    /**
-     * Must be at least 8 characters
-     *
-     * @var string $keyString
-     */
-    private $keyString;
+    private $queries = [];
 }
