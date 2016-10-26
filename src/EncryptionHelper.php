ef27f3a7c229d5ca59d1795d647c85a487327ddf
Michael Cummings <mgcummings@yahoo.com>
2016-10-26 08:03:34 -0700

    Added full correct licensing info to EncryptionHelper.

--- src/EncryptionHelper.php
+++ src/EncryptionHelper.php
@@ -5,12 +5,35 @@ declare(strict_types = 1);
  *
  * PHP version 7.0+
  *
+ * LICENSE:
+ * This file is part of Encryption Helper - Use to help with the encryption and decryption of GET queries.
  * Copyright (C) 2016 Michael Cummings
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, you may write to
+ *
+ * Free Software Foundation, Inc.
+ * 59 Temple Place, Suite 330
+ * Boston, MA 02111-1307 USA
+ *
+ * or find a electronic copy at
+ * <http://spdx.org/licenses/GPL-2.0.html>.
+ *
+ * You should also be able to find a copy of this license in the included
+ * LICENSE file.
  *
  * @author    Michael Cummings <mgcummings@yahoo.com>
  * @copyright 2016 Michael Cummings
- * @license   GPL-3.0+
- * @link      http://spdx.org/licenses/GPL-3.0.html
+ * @license   GPL-2.0
  */
 namespace EncryptionHelper;
 
